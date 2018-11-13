6a842280ea111ab9135950e303f1d6ea3d2cc07e
BornaMD <borna.djavdan@gmail.com>
2018-11-13 13:52:32 +0100

    Initial Clone

---
+++ readme.txt
@@ -0,0 +1,1 @@
+This repository contains the attempt(s) at the final exam assignment of CP4EOR in 2018. 
---
+++ try.py
@@ -0,0 +1,2 @@
+#This python file is an attempt to look at if you can connect with the Git Repository.
+print(5)
