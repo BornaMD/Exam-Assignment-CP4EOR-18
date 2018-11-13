60d98b0ef76f2ba23ebdea977afa69b08ae15e8c
BornaMD <borna.djavdan@gmail.com>
2018-11-13 14:53:30 +0100

    Borna (0)

--- try.py
+++ try.py
@@ -1,2 +1,10 @@
-#This python file is an attempt to look at if you can connect with the Git Repository.
+# This python file is an attempt to look at if you can connect with the Git Repository.
+# Change Code, push and pull changes and print code. To do the check properly please change the way you return 5 in the output.
+# The example of the output:
 print(5)
+
+# The try you should be able to change:
+def r(x):
+  return x
+
+print(r(5))
