3dad570946ac7a0cdce21548f9fdee2e93662089
Ning wei Zhou <44727781+Calcalus1991@users.noreply.github.com>
2018-11-18 16:48:36 +0100

    Update TremauxTest.py
    Still a tiny mistakes somewhere. In order to check if all things were running properly I turned out the back_track such that it only returns the matrix. From there I ran every part manually step by step. Apparently it should work, but there is still a tiny error somewhere in the code. Fix that and the tremaux should always work!

--- TremauxTest.py
+++ TremauxTest.py
@@ -10,26 +10,39 @@ for i in range(len(content)):
         linelist.append(int(element))
     maze_array.append(linelist)
 fh.close()
+row = 0
+column = 0
+width = len(maze_array[1])
+height = len(maze_array)
+starting_pos=[]
+for i in range(height):
+        for j in range(width):
+                if maze_array[i][j]==2:
+                        starting_pos.append(i)
+                        starting_pos.append(j)
+# print(maze_array)
 
 def is_dead_end(x,y,i):
-    count=0
-    if maze_array[x][y]==1:
-        return False
-    if x==0 or x==(len(maze_array)-1) or y==0 or y==(len(maze_array[0])-1):
-            return False
-    else:
-            if maze_array[x-1][y]!=0 or maze_array[x-1][y]!=i:       
-                    count+=1
-            if maze_array[x+1][y]!=0 or maze_array[x+1][y]!=i:
-                    count+=1        
-            if maze_array[x][y-1]!=0 or maze_array[x][y-1]!=i:
-                    count+=1
-            if maze_array[x][y+1]!=0 or maze_array[x][y+1]!=i:
-                    count+=1
-    if count>=3:
-        return True
-    else:
-        return False
+        count=0
+        if maze_array[x][y]==1:
+                return False
+        if x==0 or x==(len(maze_array)-1) or y==0 or y==(len(maze_array[0])-1):
+                return False
+        elif maze_array[x][y]==i and (maze_array[x+1][y]==0 or maze_array[x-1][y]==0 or maze_array[x][y-1]==0 or maze_array[x][y+1]==0):
+                return False
+        else:
+                if maze_array[x-1][y]!=0 and maze_array[x-1][y]!=i:       
+                        count+=1
+                if maze_array[x+1][y]!=0 and maze_array[x+1][y]!=i:
+                        count+=1        
+                if maze_array[x][y-1]!=0 and maze_array[x][y-1]!=i:
+                        count+=1
+                if maze_array[x][y+1]!=0 and maze_array[x][y+1]!=i:
+                        count+=1
+        if count==3:
+                return True
+        else:
+                return False
 
 def is_junction(x,y):
     count=0
@@ -51,18 +64,85 @@ def is_junction(x,y):
     else:
         return False
 
-def back_track(x,y,i):
+def Finish(x,y,u,v):
+        if x==u and y==v:
+                return True
+        else:
+                return False
+
+def back_track(x,y,u,v,i):
     maze_array[x][y]=1
     if maze_array[x-1][y]==i:
-        return back_track(x-1,y,i)
+        return back_track(x-1,y,u,v,i)
     elif maze_array[x+1][y]==i:
-        return back_track(x+1,y,i)
+        return back_track(x+1,y,u,v,i)
     elif maze_array[x][y-1]==i:
-        return back_track[x][y-1]
+        return back_track(x,y-1,u,v,i)
     elif maze_array[x][y+1]==i:
-        return back_track[x][y+1]
+        return back_track(x,y+1,u,v,i)
     else:
-        return back_track[x][y]
+        maze_array[x][y]=i-1
+        # return walking(x,y,u,v,i-1) #turn this off and run each stap manually and it will work
+        return maze_array #turn this on and run each step manually and it will work
+        
+        
+        # Still trying to figure out where the mistakes is
 
-def walking(x,y,i):
-    
+def walking(x,y,u,v,i):
+        if is_junction(x,y)==False and is_dead_end(x,y,i)==False and Finish(x,y,u,v)==False:
+                maze_array[x][y]=i 
+                if maze_array[x-1][y]==0:       
+                        return walking(x-1,y,u,v,i)
+                elif maze_array[x+1][y]==0:
+                        return walking(x+1,y,u,v,i)      
+                elif maze_array[x][y-1]==0:
+                        return walking(x,y-1,u,v,i)
+                elif maze_array[x][y+1]==0:
+                        return walking(x,y+1,u,v,i) 
+        elif is_junction(x,y)==True:
+                maze_array[x][y]=i+1
+                if maze_array[x-1][y]==0:       
+                        return walking(x-1,y,u,v,i+1)
+                elif maze_array[x+1][y]==0:
+                        return walking(x+1,y,u,v,i+1)        
+                elif maze_array[x][y-1]==0:
+                        return walking(x,y-1,u,v,i+1)
+                elif maze_array[x][y+1]==0:
+                        return walking(x,y+1,u,v,i+1)
+        elif is_dead_end(x,y,i)==True:
+                maze_array[x][y]=i
+                return back_track(x,y,u,v,i)
+        elif Finish(x,y,u,v)==True:
+                maze_array[x][y]=i 
+                return maze_array
+        
+x=starting_pos[0]
+y=starting_pos[1]
+u=6
+v=10
+j=0
+i=2
+walking(x,y,v,u,i)
+print() #Turn all these below on and make sure the upper items are on/off then turn it on and it will work
+print(maze_array)
+print(is_dead_end(5,1,3))
+walking(5,1,6,10,3)
+print()
+print(maze_array)
+print(is_junction(5,4))
+walking(5,4,6,10,3)
+print()
+print(maze_array)
+walking(5,4,6,10,3)
+print()
+print(maze_array)
+walking(3,1,6,10,2)
+print()
+print(maze_array)
+walking(1,2,6,10,2)
+print()
+print(maze_array)
+walking(4,9,6,10,2)
+print()
+print(maze_array)
+
